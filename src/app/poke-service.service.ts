54002d738f7e8a809bf09d9a24ee06662c712b94
Hugo Herrador <at_dessig21@mapa.es>
2022-05-26 10:33:07 +0200

    Rxjs res is now saved in a component private variable, this is cache in memory

--- src/app/poke-service.service.ts
+++ src/app/poke-service.service.ts
@@ -7,13 +7,15 @@ import { filter, startWith, tap } from "rxjs/operators";
 })
 export class PokeService {
 
+  private pokeCache: any;
+
   constructor(private http: HttpClient) { }
 
   all_pokemons():Observable<any> {
     return this.http.get<any>('https://pokeapi.co/api/v2/pokemon')
     .pipe(
-      tap(res => localStorage['chash_key'] = JSON.stringify(res)),
-      startWith(JSON.parse(localStorage['chash_key'] || '{}')),
+      tap(res => this.pokeCache = JSON.stringify(res)),
+      startWith(JSON.parse(this.pokeCache || '{}')),
       filter(res=>Object.keys(res).length !== 0), 
     );
     /* in case response is an array instead of object
